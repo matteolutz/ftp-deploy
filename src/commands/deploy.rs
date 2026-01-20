3a073d340e43b55ae5d17656247d86961461f4c8
Matteo Lutz <matteo.lutz06@outlook.de>
2026-01-20 16:05:45 +0100

    chore: added file deletion detection

--- src/commands/deploy.rs
+++ src/commands/deploy.rs
@@ -51,6 +51,43 @@ impl From<FilesTracking> for FileWalk {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum FileUpdateType {
+    CreateOrUpdate,
+    Deleted,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct FileUpdate {
+    file: PathBuf,
+    update_type: FileUpdateType,
+}
+
+impl FileUpdate {
+    pub fn from_updated_and_deleted(
+        updated_files: Vec<PathBuf>,
+        deleted_files: Vec<PathBuf>,
+    ) -> Vec<Self> {
+        let mut updates = Vec::with_capacity(updated_files.len() + deleted_files.len());
+
+        for file in updated_files {
+            updates.push(FileUpdate {
+                file,
+                update_type: FileUpdateType::CreateOrUpdate,
+            });
+        }
+
+        for file in deleted_files {
+            updates.push(FileUpdate {
+                file,
+                update_type: FileUpdateType::Deleted,
+            });
+        }
+
+        updates
+    }
+}
+
 #[derive(Args)]
 pub struct DeployCommand {
     /// Directory to initialize the configuration file in
@@ -77,6 +114,7 @@ impl DeployCommand {
 
         let walker = WalkBuilder::new(&base_path)
             .add_custom_ignore_filename(IGNORE_FILE_NAME)
+            .hidden(false)
             .threads(jobs)
             .build_parallel();
 
@@ -110,11 +148,7 @@ impl DeployCommand {
             .unwrap()
             .into_inner()?;
 
-        println!(
-            "[ftp-deploy] Collecting files took {:?}. {} files where updated.",
-            start.elapsed(),
-            updated_files.len()
-        );
+        println!("[ftp-deploy] Collecting files took {:?}.", start.elapsed(),);
 
         Ok((files, updated_files))
     }
@@ -122,11 +156,14 @@ impl DeployCommand {
     fn upload_files(
         &self,
         creds: &FtpCreds,
-        updated_files: Vec<PathBuf>,
+        updated_files: Vec<FileUpdate>,
     ) -> Result<(), Box<dyn std::error::Error>> {
         println!("[ftp-deploy] Uploading files to {}", creds.server);
 
         let mut ftp_stream = creds.open_stream()?;
+
+        ftp_stream.cwd("/")?;
+        let mut _current_ftp_path = PathBuf::from("/");
 
         let style = ProgressStyle::with_template(
             "{spinner:.green} [{elapsed_precise}] {msg} [{wide_bar:.cyan/blue}] ({eta})",
@@ -135,7 +172,7 @@ impl DeployCommand {
         .progress_chars("#>-");
         let pb = ProgressBar::new(updated_files.len() as u64).with_style(style);
 
-        for file in updated_files.into_iter() {
+        for FileUpdate { file, update_type } in updated_files.into_iter() {
             // TODO: sort file paths and only do necessary mkdir's and cwd's
 
             let Some(file_name) = file.file_name() else {
@@ -144,14 +181,29 @@ impl DeployCommand {
             };
 
             let ftp_path = creds.ftp_path(&file);
+            // TODO: get relative path to current path
+
             let file_name: &str = file_name.try_into().unwrap();
 
             pb.set_message(file.display().to_string());
 
             ftp_stream.cwd_or_create_recursive(ftp_path.parent())?;
 
-            let mut reader = File::open(&file)?;
-            ftp_stream.put(file_name, &mut reader)?;
+            // TODO: update current path
+
+            match update_type {
+                FileUpdateType::Deleted => {
+                    if file.is_file() {
+                        ftp_stream.rm(file_name)?;
+                    } else {
+                        ftp_stream.rmdir(file_name)?;
+                    }
+                }
+                FileUpdateType::CreateOrUpdate => {
+                    let mut reader = File::open(&file)?;
+                    ftp_stream.put(file_name, &mut reader)?;
+                }
+            }
 
             pb.inc(1);
         }
@@ -172,11 +224,32 @@ impl SubcommandDelegate for DeployCommand {
             config.run_hooks();
         }
 
-        let (files, updated_files) =
-            self.collect_files(&base_path, FilesTracking::load_or_create(&base_path)?)?;
+        let files_tracking = FilesTracking::load_or_create(&base_path)?;
+        let files_before = files_tracking
+            .files()
+            .keys()
+            .clone()
+            .into_iter()
+            .map(|path| path.clone())
+            .collect::<Vec<_>>();
 
-        if !updated_files.is_empty() {
-            self.upload_files(&creds, updated_files)?;
+        let (files, updated_files) = self.collect_files(&base_path, files_tracking)?;
+
+        let deleted_files = files_before
+            .into_iter()
+            .filter(|path| files.contains_key(path.as_path()))
+            .collect::<Vec<_>>();
+
+        println!(
+            "[ftp-deploy] {} file(s) were updated, {} file(s) were deleted",
+            updated_files.len(),
+            deleted_files.len()
+        );
+
+        let updates = FileUpdate::from_updated_and_deleted(updated_files, deleted_files);
+
+        if !updates.is_empty() {
+            self.upload_files(&creds, updates)?;
         } else {
             println!("[ftp-deploy] No files to upload.")
         }
--- src/tracking/files.rs
+++ src/tracking/files.rs
@@ -9,6 +9,12 @@ pub struct FilesTracking {
     pub(crate) files: HashMap<PathBuf, String>,
 }
 
+impl FilesTracking {
+    pub fn files(&self) -> &HashMap<PathBuf, String> {
+        &self.files
+    }
+}
+
 impl TrackingFile for FilesTracking {
     const FILE_NAME: &'static str = "files.json";
 }
